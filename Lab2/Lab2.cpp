5a3b31641b50a458b5e608a10506d9c2e6d21ac8
MoskvinMO <fpm.moskvin@bsu.by>
2026-09-22 23:41:39 +0300

    Some changes.
    Some small changes.

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0); // faster input/output; helpful for big n
+
     constexpr long long MaxSize = 1048576;
     int* arr = new int[MaxSize];
     int n{};
@@ -12,7 +14,7 @@ int main() {
 
     while(n < 1 || n > MaxSize) {
         cout << "Incorrect Input. Try again or write the same n if you are ready: ";
-        int k;
+        int k{};
         cin >> k;
         if(k == n) break;
         n = k;
@@ -25,7 +27,7 @@ int main() {
     cin >> b;
     if(b) {
         // Array input is from the keyboard.
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; ++i) {
             cout << "Element number " << i + 1 << " is: ";
             cin >> i[arr];
         }
@@ -56,11 +58,11 @@ int main() {
         Product *= *(arr + i);
     }
     // In case n == 1 there are not any even index; we write about this when n == 1.
-    cout << "\nProduct of elements with even indexes is " << Product 
+    cout << "\nProduct of elements with even indexes is: " << Product 
     << (n - 1 ? ".\n" : "   (even indexes don't exist).\n");
 
     long long Sum{};
-    int i1{}, i2{}, ArrElement;
+    int i1{}, i2{}, ArrElement{};
     bool IsZeroWas = 0;
     for(int i = 0; i < n; ++i) {
         ArrElement = arr[i];
@@ -77,10 +79,9 @@ int main() {
         While it is not ordered as we want, move negative numbers to the start.
         When moving, we scan all array, and if we see positive and then negative integers, we swap them.
     */
-        for(int i = 0; i < n - 1; i++){
+        for(int i = 0; i < n - 1; ++i) {
             if(arr[i + 1] < 0 && arr[i] >= 0) {
                 // swap arr[i] and arr[i+1]
-                // For some reason, built-in swap doesn't work.
                 arr[i] ^= arr[i + 1];
                 arr[i + 1] ^= arr[i];
                 arr[i] ^= arr[i + 1];
@@ -94,13 +95,13 @@ int main() {
         */
         bool IsSmth = 0;
         IsFinished = 1;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; ++i) {
             if(arr[i] >= 0){IsSmth = 1; continue;}
             if(arr[i] < 0 && IsSmth){IsFinished = 0; break;}
         }
     }
     cout << "\nModified array:\n";
-    for(int i = 0; i < n - 1; i++) cout << arr[i] << '\x20';
+    for(int i = 0; i < n - 1; ++i) cout << arr[i] << '\x20';
     cout << arr[n - 1] << "\n\n";
 
     delete[] arr;
